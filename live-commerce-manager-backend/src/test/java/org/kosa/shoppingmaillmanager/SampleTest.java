8038e31545a1efefe1009522ecfe359dba9055f4
kimseokryul <kimseokryul96@gmail.com>
2025-07-13 17:11:25 +0900

    07.13 테스트 코드 변경

---
+++ live-commerce-manager-backend/src/test/java/org/kosa/shoppingmaillmanager/SampleTest.java
@@ -0,0 +1,12 @@
+package org.kosa.shoppingmaillmanager;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    @Test
+    void basicTest() {
+        assertEquals(2, 1 + 1);  // ✅ 무조건 통과
+    }
+}
